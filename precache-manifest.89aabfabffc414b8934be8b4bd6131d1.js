fd45368846588faf1b79b9a2476cfb5bd9952c17
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-07-02 11:15:02 -0600

    Updates

---
+++ precache-manifest.89aabfabffc414b8934be8b4bd6131d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8904a99f400c526c0d1c90f213fbe3",
+    "url": "/moviegoers/index.html"
+  },
+  {
+    "revision": "da1a74a4b3e7ad77719b",
+    "url": "/moviegoers/static/css/main.8e9b7c07.chunk.css"
+  },
+  {
+    "revision": "cf3f1a7055cd78f49087",
+    "url": "/moviegoers/static/js/2.03a8b360.chunk.js"
+  },
+  {
+    "revision": "da1a74a4b3e7ad77719b",
+    "url": "/moviegoers/static/js/main.9c050f6e.chunk.js"
+  },
+  {
+    "revision": "082fd7d42cab15cd62a7",
+    "url": "/moviegoers/static/js/runtime~main.a5dcd277.js"
+  }
+]);
